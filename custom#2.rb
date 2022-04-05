7afdb4f98ddb96b0153bd788960cedfe3ed38aae
ericcanton1 <99507629+ericcanton1@users.noreply.github.com>
2022-04-05 12:13:36 -0700

    Create custom#2.rb

---
+++ custom#2.rb
@@ -0,0 +1,259 @@
+use_bpm 65.5
+#Undertale
+
+#define :dif do |n1, n2, n3]
+live_loop :q do
+  1.times do
+    use_synth :tri
+    #m1
+    play :c4, release: 2
+    sleep 1
+    play :c5, release: 2
+    sleep 1
+    play :g4, release: 2.5
+    sleep 2
+    #m2
+    play :f4, release: 2
+    sleep 1
+    play :c5, release: 2
+    sleep 1
+    play :c4, release: 3
+    sleep 2
+    #m3
+    play :c4, release: 2.5
+    sleep 1
+    play :f4, release: 3
+    sleep 1
+    play :c5, release: 3
+    sleep 1
+    play :d5, release: 3
+    sleep 1
+    #m4
+    play :c5, release: 3
+    sleep 1
+    play :g4, release: 2
+    sleep 1
+    play :f4, release: 2
+    sleep 2
+  end
+  stop
+end
+
+live_loop :s do
+  4.times do
+    play :d3, release: 2
+    sleep 2
+    play :f3, release: 2
+    sleep 2
+    play :e3, release: 2
+    sleep 2
+    play :e3, release: 2
+    sleep 2
+  end
+  stop
+end
+
+
+
+sleep 16
+
+live_loop :o do
+  1.times do
+    use_synth :tri
+    #m1
+    play :c4, release: 2
+    sleep 1
+    play :c5, release: 2
+    sleep 1
+    play :g4, release: 2.5
+    sleep 2
+    #m2
+    play :f4, release: 2
+    sleep 1
+    play :c5, release: 2
+    sleep 1
+    play :c4, release: 3
+    sleep 2
+    #m3
+    play :c4, release: 2.5
+    sleep 1
+    play :f4, release: 3
+    sleep 1
+    play :c5, release: 3
+    sleep 1
+    play :d5, release: 3
+    sleep 1
+  end
+  stop
+end
+
+sleep 12
+
+live_loop :j do
+  1.times do
+    use_synth :tri
+    #m5
+    play :c5, release: 2
+    sleep 1
+    play :g4, release: 2
+    sleep 1
+    play :f4, release: 2.75
+    sleep 1.5
+    play :r, release: 0.5
+    sleep 0.25
+    play :as3, release: 0.3
+    sleep 0.125
+    play :b3, release: 0.3
+    sleep 0.125
+  end
+  stop
+end
+
+sleep 4
+
+
+live_loop :a do
+  2.times do
+    use_synth :tri
+    #m6
+    play :c4, release: 2
+    sleep 1
+    play :c5, release: 1
+    sleep 0.5
+    play :a4, release: 1
+    sleep 0.5
+    play :g4, release: 1
+    sleep 0.5
+    play :f4, release: 1
+    sleep 0.5
+    play :e4, release: 1
+    sleep 0.5
+    play :f4, release: 1
+    sleep 0.5
+    #m7
+    play :g4, release: 2
+    sleep 1
+    play :e5, release: 2
+    sleep 0.75
+    play :ds5, release: 0.3
+    sleep 0.125
+    play :d5, release: 0.3
+    sleep 0.125
+    play :c5, release: 2
+    sleep 2
+    #m8
+    play :d4, release: 2
+    sleep 1
+    play :c5, release: 1
+    sleep 0.5
+    play :a4, release: 1
+    sleep 0.5
+    play :g4, release: 1
+    sleep 0.5
+    play :f4, release: 1
+    sleep 0.5
+    play :g4, release: 1
+    sleep 0.5
+    play :a4, release: 1
+    sleep 0.5
+    #m9
+    play :e4, release: 2
+    sleep 1
+    play :g4, release: 2
+    sleep 1
+    play :g4, release: 1
+    sleep 0.5
+    play :f4, release: 1
+    sleep 0.5
+    play :e4, release: 1
+    sleep 0.5
+    play :f4, release: 1
+    sleep 0.5
+  end
+  stop
+end
+
+sleep 32
+
+live_loop :h do
+  2.times do
+    use_synth :tri
+    #m10
+    play :c6, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :b4, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    #m11
+    play :b4, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :b5, release: 1
+    sleep 0.5
+    play :a5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :c5, release: 1
+    sleep 0.5
+  end
+  stop
+end
+
+live_loop :ll do
+  2.times do
+    use_synth :tri
+    #fade out
+    #m10
+    play :c6, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :b4, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    #m11
+    play :b4, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :b5, release: 1
+    sleep 0.5
+    play :a5, release: 1
+    sleep 0.5
+    play :g5, release: 1
+    sleep 0.5
+    play :f5, release: 1
+    sleep 0.5
+    play :c5, release: 1
+    sleep 0.5
+  end
+  stop
+end
